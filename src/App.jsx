0a3fe7275c9b5417d0d5198e4733070224c52dac
Ananthakrishna <ananthaak24@gmail.com>
2023-12-22 20:58:45 +0530

    initial setup and commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Background from "./components/background";
+
+function App() {
+  return (
+    <div className="bg-zinc-800 w-full h-screen relative">
+      <Background />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Background.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Background() {
+  return (
+    <>
+      <div className="text-xl flex justify-center text-zinc-600 py-8 w-full absolute top-[4%]">
+        Documents
+      </div>
+      <h1 className="text-[13vh] leading-none tracking-lighter absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900">
+        DOCS.
+      </h1>
+    </>
+  );
+}
+
+export default Background;
